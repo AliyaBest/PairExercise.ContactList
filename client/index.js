3f6ac1add45a22dff205d9ae962e430fd3ff3604
AliyaBest <aliyabest@gmail.com>
2020-05-05 17:47:43 -0400

    completed workshop

--- client/index.js
+++ client/index.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import ContactList from './ContactList';
+import Axios from 'axios';
 
-const contacts = [
-  { "id": 1, "name": "R2-D2", "phone": "222-222-2222", "email": "r2d2@droids.com" },
-  { "id": 2, "name": "C-3PO", "phone": "333-333-3333", "email": "c3po@droids.com" },
-  { "id": 3, "name": "BB-8", "phone": "888-888-8888", "email": "bb8@droids.com" }
-]
+// const c = [
+//   { "id": 1, "name": "R2-D2", "phone": "222-222-2222", "email": "r2d2@droids.com" },
+//   { "id": 2, "name": "C-3PO", "phone": "333-333-3333", "email": "c3po@droids.com" },
+//   { "id": 3, "name": "BB-8", "phone": "888-888-8888", "email": "bb8@droids.com" }
+// ]
 
 class Main extends Component {
   constructor() {
     super();
     this.state = {
-      contacts: contacts
+      contacts: []
     }
   }
+
+  async componentDidMount(){
+    try{
+      const res = await axios.get('/api/contacts')
+      const contacts = res.data
+      this.setState({contacts})
+    } catch (err){
+      console.log('There was a problem making contact')
+    }
+  }
+
   render() {
     return (
       <div id='main'>
